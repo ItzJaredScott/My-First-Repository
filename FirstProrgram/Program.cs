3081159624dac0cc35ffeef0a8f1a81f282bd562
ImHawkeye <167693724+ImHawkeye@users.noreply.github.com>
2025-09-16 23:28:47 -0500

    Added comments to the program

--- FirstProrgram/Program.cs
+++ FirstProrgram/Program.cs
@@ -4,20 +4,20 @@
     {
         static void Main(string[] args)
         {
-            string firstName, lastName, fullName;
+            string firstName, lastName, fullName; // defines each variable as a string
             Console.WriteLine("What is your First name?");
             firstName = Console.ReadLine();
             Console.WriteLine("What is your Last name?");
             lastName = Console.ReadLine();
-            fullName = firstName + " " + lastName;
+            fullName = firstName + " " + lastName; // turns first and last name strings into one fullname string
             Console.WriteLine("Hello " + fullName);
-            Console.WriteLine("Give me a number!");
-            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Give me a number!"); 
+            double x = Convert.ToDouble(Console.ReadLine()); // converts the number given to a double so decimals are accepted.
             Console.WriteLine("Give me a second number!");
             double y = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Give me a third number!");
             double z = Convert.ToDouble(Console.ReadLine());
-            double a = x + y + z;
+            double a = x + y + z; 
             Console.WriteLine("If you add those numbers together it equals " + a);
             Console.WriteLine("If you subtract the second number from the first, it equals " + (x -
             y));
